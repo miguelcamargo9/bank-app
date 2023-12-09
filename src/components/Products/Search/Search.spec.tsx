ed452582bb59baf023c8e9c6e1925cbea7dc3237
Miguel Camargo <miguelcamargo9@gmail.com>
2023-12-09 02:33:07 -0500

    fix: broken compillation

--- src/components/Products/Search/Search.spec.tsx
+++ src/components/Products/Search/Search.spec.tsx
@@ -16,10 +16,10 @@ describe("Search component", () => {
   });
 
   it("updates on input change", () => {
-    const inputEl = screen.getByPlaceholderText("Buscar productos financieros");
+    const inputEl = screen.getByPlaceholderText("Buscar productos financieros") as HTMLInputElement;
     fireEvent.change(inputEl, { target: { value: "test query" } });
     expect(inputEl.value).toBe("test query");
-  });
+  });  
 
   it("calls onSearch on input change", () => {
     const inputEl = screen.getByPlaceholderText("Buscar productos financieros");
